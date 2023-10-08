8fe4bc3664f6f0b5ffa5ab9bcde6f6d0e70fde5b
NDivyaReddy <147125549+NDivyaReddy@users.noreply.github.com>
2023-10-08 20:59:47 +0530

    Add files via upload

---
+++ 06-10-23/Lab01.py
@@ -0,0 +1,26 @@
+c="A"
+print(c)
+name="Divya"
+print(name)
+
+###########################
+num1=input("enter your Number1")
+num2=input("enter your Number2")
+print(num1,num2)
+
+print(num1+num2)
+
+# convert string to number
+num1=int(input("enter your Number1/n"))
+num1=int(num1)
+print(type(num1))
+
+###########Length
+name='Divya'
+print(len(name))
+############Index
+print(name[4])
+###Slice
+print(name[0:2])##start from Zero and print 2 characters
+print(name[2:4])##start from 2nd and print 4 characters
+
---
+++ 06-10-23/Taks.py
@@ -0,0 +1,28 @@
+#Printing the table
+n=input('Enter number\n')
+n=int(n)
+print(f'Table of {n} is :')
+
+print(f"{n} X 1 = {n*1}")
+print(f"{n} X 2 = {n*2}")
+print(f"{n} X 3 = {n*3}")
+print(f"{n} X 4 = {n*4}")
+print(f"{n} X 5 = {n*5}")
+print(f"{n} X 6 = {n*6}")
+print(f"{n} X 7 = {n*7}")
+print(f"{n} X 8 = {n*8}")
+print(f"{n} X 9 = {n*9}")
+print(f"{n} X 10 = {n*10}")
+
+#List the all the functions avaiable for the String Data Type.
+text='Divya'
+print(len(text))
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.swapcase())
+print(text.capitalize())
+print(text.find("i"))
+text1=" My name is this"
+print(text1.replace("this","divya"))
+
